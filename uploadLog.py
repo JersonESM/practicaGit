5a1f388ede54f0b54b5abd135a1ad6284e9bedd7
JersonESM <jerson_ordaz@esmindustries.com.mx>
2021-11-23 13:15:07 -0600

    se obtienen los valores necesarios para trabajar

--- uploadLog.py
+++ uploadLog.py
@@ -1,6 +1,24 @@
 import requests
 import json
 import os
+import os.path, time
+import datetime
+import pathlib
+
+fileSize = os.stat(r'C:\Users\jerso\Desktop\Empresas\InnovaSport\proyectoGit\raspcontrol\js\log_18_Nov.log')
+print('File Size is', fileSize.st_size, 'bytes')
+
+fname = pathlib.Path(r'C:\Users\jerso\Desktop\Empresas\InnovaSport\proyectoGit\raspcontrol\js\log_18_Nov.log')
+dateCreate = datetime.datetime.fromtimestamp(fname.stat().st_ctime)
+print(dateCreate)
+
+now = datetime.datetime.now()
+print(now)
+
+daysOnRasp = abs(now - dateCreate)
+print(daysOnRasp.days)
+
+
 
 def is_json(myjson):
     try:
@@ -41,3 +59,9 @@ except ValueError as e:
 #Condicion de que si no se ha subido y pesa mas de 6MB que borre el programa si es que no se ha subido en 2 días 
 #Cuando subes el archivo por ende se borra para crear uno nuevo 
 #Expresion crontab 30 1 0,2,5,9,11,15,17,21,23 ? * * *
+
+
+#1. Valuar la fecha de creación y fecha actual
+#2. Valuar el peso del archivo a subir
+#3. Condición si la fecha es menos de 2 días y el archivo pesa menos de 6MB subir, si no borar
+#4. Dentro del if intentar que se suba varias veces el archivo si se sube borrar el archvio
